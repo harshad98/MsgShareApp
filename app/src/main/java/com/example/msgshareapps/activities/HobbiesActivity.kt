d9c3c90134c94c9e03e67ee5c3423147f1e4dabd
harshad98 <harshadraut98@gmail.com>
2019-05-11 15:23:16 +0530

    my commit

---
+++ app/src/main/java/com/example/msgshareapps/activities/HobbiesActivity.kt
@@ -0,0 +1,38 @@
+package com.example.msgshareapps.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import com.example.msgshareapps.adapters.HobbiesAdapter
+import com.example.msgshareapps.R
+import com.example.msgshareapps.models.Supplier
+import kotlinx.android.synthetic.main.activity_hobbies.*
+
+
+
+
+
+
+class HobbiesActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG: String = HobbiesActivity::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hobbies)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
+
+        val adapter= HobbiesAdapter(this, Supplier.hobbies)
+        recyclerView.adapter = adapter
+    }
+}
---
+++ app/src/main/java/com/example/msgshareapps/activities/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.msgshareapps.activities
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.msgshareapps.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity","Buuton was clocked!!")
+            Toast.makeText(this, "button was clicked",Toast.LENGTH_SHORT).show()
+        }
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message:String = etUserMessage.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+            val intent= Intent(this, SecondActivity::class.java)
+            intent.putExtra("user_message",message)
+            startActivity(intent)
+        }
+        btnShareToOtherApps.setOnClickListener {
+            val message:String = etUserMessage.text.toString()
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT,message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent,"Share to:"))
+        }
+        btnRecyclerViewDemo.setOnClickListener {
+            val intent=Intent(this, HobbiesActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+}
+
